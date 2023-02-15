8564d632e10b2cb9151d621a9006c74c2bf6c370
korarit pannopasri <nhuneqw@gmail.com>
2023-02-15 17:59:13 +0700

    เหลือเงื่อนไขนับแต้ม

--- Poker.cpp
+++ Poker.cpp
@@ -1,17 +1,155 @@
 #include <iostream>
-#include <cstdlib>
-#include <string>
-#include <iomanip>
+#include <vector>
 #include <algorithm>
 #include <ctime>
+#include <cstdlib>
+#include <random>
 
 using namespace std;
 
-string suit[] = {"\3", "\4", "\5", "\6"};
-string rank[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+//ทำstruct เก็บ ชนิดกับค่าของไฟ่
+struct Card {
+    string rank;
+    string suit;    
+};
 
+//ฟังก์ชั่นสร้างdeckและกำหนดค่าต่างๆในvector
+vector<Card> create_deck() {
+    vector<Card> deck;
+    string suits[] = {"H", "D", "C", "S"};
+    string ranks[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    for (string suit : suits) {
+        for (string rank : ranks) {
+            deck.push_back({rank, suit});
+        }
+    }
+    return deck;
+}
 
+//shuffle deck ทั้งกอง
+void shuffle_deck(vector<Card>& deck) {
+    random_device rd;
+    mt19937 g(rd()); //ลองไปศึกษาดู ส่วนตัวอย่างนี้สะดวกกว่า
+    shuffle(deck.begin(), deck.end(), g);
+}
+
+//ฟังก์ชั่นโชว์การ์ด
+void Show_Community_Card(string R, vector<Card>& deck) {
+    int n = deck.size();
+    cout << "Community Card " ;
+    if (R == "flop") {
+        for (int i = 0; i < 3; i++) {
+            cout<< deck[n - i - 1].rank << deck[n - i - 1].suit << " ";
+        }
+    }
+    else if (R == "turn") {
+        for (int i = 0; i < 4; i++) {
+            cout<< deck[n - i - 1].rank << deck[n - i - 1].suit << " ";
+        }
+    }
+    else {
+        for (int i = 0; i < 5; i++) {
+            cout<< deck[n - i - 1].rank << deck[n - i - 1].suit << " ";
+        }
+    }
+    cout << endl;
+}
+
+//ฟังก์ชั่นคำนวณchipที่ลงเดิมพัน
+void Bet(int& chip, int& all_bet) {
+    int bet;
+    while (true) {
+        cout << "Bet: ";
+        cin >> bet;
+        if (chip - bet >= 0) {
+            chip -= bet;
+            all_bet += 2 * bet;
+            break;
+        }
+        else {
+            cout << "Not enough chips." << endl;
+        }
+    }
+}
+
+//ฟังก์ชั่นตัวเลือกต่างๆ
+void Choose(int& chip, int& all_bet,vector<Card> dealer_hand) {
+    char choose;
+    cout << "Fold/Call/All in -->(F/C/A): ";
+    do {
+        cin >> choose;
+        if (choose == 'F') {//หมอบ_แต่เล่น2คนก็เหมือนจบเกม
+            cout << "You folded." << endl;
+            cout << "Dealer Card :: " << dealer_hand[0].rank << dealer_hand[0].suit << " " << dealer_hand[1].rank << dealer_hand[1].suit << endl;
+            cout << "Game over." << endl;
+            exit(0);
+        }
+        else if (choose == 'C') {//ลงเดิมพันต่อ
+            Bet(chip, all_bet);
+        }
+        else if (choose == 'A') {//หมดหน้าตัก
+            all_bet += 2 * chip;
+            chip = 0;
+            cout << "You are all in." << endl;
+        }
+    } while (choose != 'F' && choose != 'C' && choose != 'A');
+}
+
+//ตามชื่อฟังก์ขั่น
+void Check_Winer(int& chip, int& all_bet, int winner) {
+    if (winner == 1) {
+        chip += all_bet;
+        cout << "You won " << all_bet << " chips." << endl;
+    }
+    else {
+        cout << "You loss "<<endl;
+    }
+}
 
 int main(){
-    srand(time(0));
+
+    int Chip=500,All_Bet=0;
+    vector<Card> deck = create_deck();
+    shuffle_deck(deck);
+
+    vector<Card> player_hand;
+    vector<Card> dealer_hand;
+
+    //จั่วการ์ด
+    for(int i=0;i<2;i++){
+        player_hand.push_back(deck.back());
+        deck.pop_back();
+        dealer_hand.push_back(deck.back());
+        deck.pop_back();
+    }
+
+    //preflop_เหมือนรอบ0นั้นแหละ
+    cout<<"Your Chip ::"<<Chip<<endl;
+    cout<<"Your Card :: "<<player_hand[0].rank<<player_hand[0].suit<<" "<<player_hand[1].rank<<player_hand[1].suit<<endl;
+    Bet(Chip,All_Bet);
+
+    //flop_เหมือนรอบแรกนั้นแหละจะลงเท่าไหร่หรือหมอบ
+    Show_Community_Card("flop",deck);
+    Choose(Chip,All_Bet,dealer_hand);
+
+    //ตามข้างบนรอบ2
+    Show_Community_Card("turn",deck);
+    Choose(Chip,All_Bet,dealer_hand);
+
+    //ตามข้างบนรอบจบ_เปิดการ์ดทุกคนแล้วนับแต้ม
+    cout<<"Dealer Card :: "<<dealer_hand[0].rank<<dealer_hand[0].suit<<" "<<dealer_hand[1].rank<<dealer_hand[1].suit<<endl;
+    Show_Community_Card("river",deck);
+    cout<<"Your Card :: "<<player_hand[0].rank<<player_hand[0].suit<<" "<<player_hand[1].rank<<player_hand[1].suit<<endl;
+
+    //ตามที่เราคุยกันไว้คือจะเอาเข้ามาในมือให้เรียบร้อย_เหมือนทั้ง2คนมี่7ใบ_แล้วทำเงื่อนไขว่าใครชนะ
+    for(int i=0;i<5;i++){
+        player_hand.push_back(deck.back());
+        dealer_hand.push_back(deck.back());
+        deck.pop_back();
+    }
+
+
+    //ฉะนั้น_ฟังค์ชั่นเงื่อนไขกับจบ_เพื่อน_ฝากที_ราตรีสวัสดิ์!!!
+
+    return 0;
 }
