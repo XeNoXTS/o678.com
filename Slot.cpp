2525b7807ba63818f85b05245678c2010cc57041
NMBxNIGHT <thiranut_kakanmee@cmu.ac.th>
2023-02-17 13:10:52 +0700

    เพิ่ม symbol

--- Slot.cpp
+++ Slot.cpp
@@ -9,32 +9,39 @@ using namespace std;
 -Slot size 3*5 
 -rand symbols
 -check win cond
+-P_input
 */
 
 
-
-int machine[3][5];
+//S==Slot
+string S_machine[3][5];
+string S_symbol[]={"\3","\4","10","\6","A","J","Q","K"};
 int P_chips = 500;
 
-string P_input(){ //P == Player
 
-    cout << "Current chips = " << P_chips << endl;
-    cout << "";
-    cin >> input; 
-
-}
-
-void randSlot(){
-    for(int i=0;i<3;i++){
-        for(int j=0;j<5;j++){
-            machine[i][j] = rand()%6;
-            cout << machine[i][j] << " ";
-        }
+void randSlot(int N){
+    for (int k = 0; k < N; k++)
+    {
+        cout << endl << "Round :" << k+1 << endl;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<5;j++){
+                S_machine[i][j] = S_symbol[(rand()%8)];
+                cout << S_machine[i][j] << " ";
+            }
         cout << endl;
+        }
+        cout << "______________________" << endl;
     }
-
 }
 
+void P_input(){ //P == Player
+    int round;
+    cout <<"Current chips = " << P_chips << endl;
+    cout << "Input amount of round to spins: ";
+    
+    cin >> round; 
+    randSlot(round);
+}
 
 int main(){
     srand(time(0));
